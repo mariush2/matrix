3790db96379bf877007c24d8e40574c963f8dfd7
mariush2 <marius@continyou.no>
2017-10-17 20:23:32 +0200

    Added movement to the left and right with increments in size

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
-let size = 22;
+let size = 32;
 let main = [];
 let string_length;
 let charSet = []
+let xSet = [size, -size, size * 2, -size * 2];
 
 for (let k = 0; k < 100; k++) {
   charSet[k] = 19984 + k;
@@ -21,11 +22,17 @@ function draw() {
   for (var i = 0; i < main.length; i++) {
     let current = main[i];
     for (var j = 0; j < current.length; j++) {
+      if (current[j].x > width + size) {
+        current[j].x = -size;
+      } else if (current[j].x < -size) {
+        current[j].x = width + size;
+      }
+
       if (current[j].y > height + size) {
         current[j].y = -size;
+        current[j].x += current[j].speed_x;
       } else {
-        let speed = map(mouseX, 0, width, 1, current[j].speed);
-        current[j].y += speed;
+        current[j].y += map(mouseX, 0, width, 1, current[j].speed_y);
       }
       if (random(0, 1) < 0.001) {
         current[j].ch = char(charSet[j]);
@@ -54,13 +61,15 @@ function init() {
   for (var i = 0; i < windowWidth / size; i++) {
     let list = []
     let randomHeight = random(0, -800);
-    let randomSpeed = random(3, 7);
+    let randomSpeed1 = random(3, 5);
+    let randomX = round(random(0, 3), 1);
     string_length = 10 + parseInt(random(1, 5));
     for (var j = 0; j < string_length; j++) {
       list[j] = new Item(j);
       list[j].x = i * size;
       list[j].y = randomHeight + j * size;
-      list[j].speed = randomSpeed;
+      list[j].speed_y = randomSpeed1;
+      list[j].speed_x = xSet[randomX];
       if (j == string_length - 1) {
         list[j].color = [100, 60, 100];
         list[j].last = true;
